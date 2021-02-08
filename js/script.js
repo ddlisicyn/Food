20d708cfa2e0c18b04e0e013a0d47ad1d4a42ff4
ddlisicyn <ddlisicyn@mail.ru>
2021-02-08 14:38:05 +0500

    commit with json-server

--- js/script.js
+++ js/script.js
@@ -248,11 +248,6 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            /* const request = new XMLHttpRequest(); Создание объекта XMLHttpRequest
-            request.open('POST', 'server.php'); */
-            
-            /* request.setRequestHeader('Content-type', 'application/json'); */
-
             const formData = new FormData(form);
 
             const object = {};
@@ -260,10 +255,6 @@ window.addEventListener('DOMContentLoaded', () => {
             formData.forEach(function(value, key) {
                 object[key] = value;
             });
-
-    /*             const json = JSON.stringify(object); */
-
-            /* request.send(json); */
 
             fetch('server.php', {
                 method: "POST",
@@ -283,37 +274,6 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
-
-
- /*             request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
-        }); */
-
- /*             request.send(formData);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    statusMessage.textContent = message.success;
-                    form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
-                } else {
-                    statusMessage.textContent = message.failure;
-                }
-            });
-        });
-     } */
-    
 
      function showThanksModal(message) {
         const prevModalDialog = document.querySelector('.modal__dialog');
@@ -339,5 +299,9 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
 
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
 
 });
